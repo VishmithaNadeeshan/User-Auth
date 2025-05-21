8d28d7332bf4b255fa30918620cfdf619ba05ed7
VishmithaNadeeshan <vishmithanadeeshan20041111@gmail.com>
2025-05-22 02:08:23 +0530

    Create a middleware to verify the JWT

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const authRoutes = require('./auth');
 
 const app = express();
 app.use(bodyParser.json());
+
+app.use('/api', authRoutes); 
 
 app.get('/', (req, res) => {
   res.send('Server is running!');
